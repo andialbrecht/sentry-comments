f751814105572b239c80c2b21603de9e72d84a7b
Andi Albrecht <albrecht.andi@gmail.com>
2013-08-09 08:44:12 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst') as f:
 
 setup(
     name='sentry-comments',
-    version='0.1.0',
+    version='0.2.0',
     author='Andi Albrecht',
     author_email='albrecht.andi@gmail.com',
     url='https://github.com/andialbrecht/sentry-comments',
